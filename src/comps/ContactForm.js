8bd38927a7ebb542deb5639fe5c325beee56708f
Daniel Anzola <dananzola@gmail.com>
2023-06-02 15:55:41 -0400

    update contact form

--- src/comps/ContactForm.js
+++ src/comps/ContactForm.js
@@ -22,26 +22,31 @@ const ContactForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="form-container">
-            <div className="form-group m-1">
-                <input type="text" className="form-control" id="transBG" name="name" value={formData.name} onChange={handleChange} placeholder="Name" required />
-            </div>
-            <div className="form-group m-1">
-                <input id="transBG" type="email" className="form-control" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
-            </div>
-            <div className="form-group m-1">
-                <input id="transBG" type="text" className="form-control" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" required />
-            </div>
-            <div className="form-group m-1">
-                <textarea id="transBG" className="form-control" rows="5" name="message" value={formData.message} onChange={handleChange} placeholder="Leave us a short message." required ></textarea>
-            </div>
-            <div className="button-container">
-                <button type="submit" className="btn-white span-angle m-0" id="titles" >
-                    <h5>send</h5>
-                </button>
-            </div>
-            {isSent && <p>Email sent successfully!</p>}
-        </form>
+        <>
+            {isSent ? (
+                <p>Email sent successfully!</p>
+            ) : (
+                <form onSubmit={handleSubmit} className="form-container">
+                    <div className="form-group m-1">
+                        <input type="text" className="form-control" id="transBG" name="name" value={formData.name} onChange={handleChange} placeholder="Name" required />
+                    </div>
+                    <div className="form-group m-1">
+                        <input id="transBG" type="email" className="form-control" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
+                    </div>
+                    <div className="form-group m-1">
+                        <input id="transBG" type="text" className="form-control" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" required />
+                    </div>
+                    <div className="form-group m-1">
+                        <textarea id="transBG" className="form-control" rows="5" name="message" value={formData.message} onChange={handleChange} placeholder="Leave us a short message." required ></textarea>
+                    </div>
+                    <div className="button-container">
+                        <button type="submit" className="btn-white span-angle m-0" id="titles" >
+                            <h5>send</h5>
+                        </button>
+                    </div>
+                </form>
+            )}
+        </>
     );
 };
 
