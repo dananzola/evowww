439a9374562c8151869379f97cadb4414bef62de
Daniel Anzola <dananzola@gmail.com>
2023-06-02 15:37:27 -0400

    emaildone

--- src/comps/ContactForm.js
+++ src/comps/ContactForm.js
@@ -1,5 +1,5 @@
 ﻿import React, { useState } from 'react';
-
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
     const [formData, setFormData] = useState({ name: '', email: '', phone: '', message: '' });
@@ -7,27 +7,13 @@ const ContactForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        fetch('http://localhost:3001/send-email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Email sending failed');
-                }
-            })
-            .then((data) => {
-                console.log(data);
+        emailjs.sendForm('service_c5234pq', 'template_2kvv8rs', e.target, 'riP2GHBEq6HYR7nqr')
+            .then((result) => {
+                console.log(result.text);
                 // Reset form after sending email
                 setFormData({ name: '', email: '', phone: '', message: '' });
-            })
-            .catch((error) => {
-                console.error('Error sending email:', error);
+            }, (error) => {
+                console.log(error.text);
             });
     };
 
@@ -38,65 +24,23 @@ const ContactForm = () => {
     return (
         <form onSubmit={handleSubmit} className="form-container">
             <div className="form-group m-1">
-                <input
-                    type="text"
-                    className="form-control"
-                    id="transBG"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder="Name"
-                    required
-                />
+                <input type="text" className="form-control" id="transBG" name="name" value={formData.name} onChange={handleChange} placeholder="Name" required />
             </div>
             <div className="form-group m-1">
-                <input
-                    id="transBG"
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    required
-                />
+                <input id="transBG" type="email" className="form-control" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
             </div>
             <div className="form-group m-1">
-                <input
-                    id="transBG"
-                    type="text"
-                    className="form-control"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    placeholder="Phone Number"
-                    required
-                />
+                <input id="transBG" type="text" className="form-control" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" required />
             </div>
             <div className="form-group m-1">
-                <textarea
-                    id="transBG"
-                    className="form-control"
-                    rows="5"
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    placeholder="Leave us a short message."
-                    required
-                ></textarea>
+                <textarea id="transBG" className="form-control" rows="5" name="message" value={formData.message} onChange={handleChange} placeholder="Leave us a short message." required ></textarea>
             </div>
             <div className="button-container">
-                <button
-                    type="submit"
-                    className="btn-white span-angle m-0"
-                    id="titles"
-                >
+                <button type="submit" className="btn-white span-angle m-0" id="titles" >
                     <h5>send</h5>
                 </button>
             </div>
         </form>
-
-
     );
 };
 
